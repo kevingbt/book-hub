3188110637c1ff9fcc8c28a3aee834643f60a187
mmejjiou-boop <mmejjiou@gmail.com>
2026-04-23 15:50:16 +0200

    ajout de l'api

--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -9,7 +9,6 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class BookType extends AbstractType
 {
