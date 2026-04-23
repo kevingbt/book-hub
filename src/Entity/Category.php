4e440314804d76d863dbed1200a9ece86175874b
Kevin Guibert <kevin.gbt2805@gmail.com>
2026-04-23 11:52:54 +0200

    ajout relation

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
@@ -15,6 +17,17 @@ class Category
 
     #[ORM\Column(length: 255)]
     private ?string $name = null;
+
+    /**
+     * @var Collection<int, Book>
+     */
+    #[ORM\ManyToMany(targetEntity: Book::class, mappedBy: 'book_category')]
+    private Collection $category_book;
+
+    public function __construct()
+    {
+        $this->category_book = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +42,33 @@ class Category
     public function setName(string $name): static
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Book>
+     */
+    public function getCategoryBook(): Collection
+    {
+        return $this->category_book;
+    }
+
+    public function addCategoryBook(Book $categoryBook): static
+    {
+        if (!$this->category_book->contains($categoryBook)) {
+            $this->category_book->add($categoryBook);
+            $categoryBook->addBookCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCategoryBook(Book $categoryBook): static
+    {
+        if ($this->category_book->removeElement($categoryBook)) {
+            $categoryBook->removeBookCategory($this);
+        }
 
         return $this;
     }
