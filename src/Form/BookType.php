5eabe38001f9a741e1015bdf467685751399ed38
Kevin Guibert <kevin.gbt2805@gmail.com>
2026-04-24 11:02:32 +0200

    fix multiple

--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Author;
 use App\Entity\Book;
 use App\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
